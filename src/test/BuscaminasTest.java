e87b4e2d83db39cb21ecbe6f7a81e4edbe948fab
Felipe Garcia <felipegarcial64@gmail.com>
2019-08-12 00:05:40 -0500

    Se avanza en las pruebas

--- src/test/BuscaminasTest.java
+++ src/test/BuscaminasTest.java
@@ -6,7 +6,45 @@ import modelo.Casilla;
 
 public class BuscaminasTest extends TestCase{
 	private Buscaminas buscaminas;
-	private Casilla casilla;
+	private Casilla[][] casillas;
+	private int nivel = 1;
+	private void setupEsceneario(int nivel) {
+		buscaminas = new Buscaminas(nivel);
+	}
 	
+	private int numeroCasillasNivel(int nivel) {
+		int casillas = 0; 
+		if(nivel == 1) {
+			casillas = 64;
+		}else if(nivel == 2) {
+			casillas = 256;
+		}else if(nivel == 3) {
+			casillas = 480;
+		}
+		return casillas;
+	}
 	
+	private int numeroMinasNivel(int nivel) {
+		int minas = 0; 
+		if(nivel == 1) {
+			minas = 10;
+		}else if(nivel == 2) {
+			minas = 40;
+		}else if(nivel == 3) {
+			minas = 90;
+		}
+		return minas;
+	}
+	
+	public void testInicializarPartida() {
+		setupEsceneario(nivel);
+		boolean condition = buscaminas.darCasillas().length * buscaminas.darCasillas()[0].length == numeroCasillasNivel(nivel); 
+		assertTrue(condition);
+	}
+	
+	public void testGenerarMinas() {
+		setupEsceneario(nivel);
+		boolean condition = buscaminas.darCasillas().length * buscaminas.darCasillas()[0].length == numeroCasillasNivel(nivel); 
+		assertTrue(condition);
+	}
 }
