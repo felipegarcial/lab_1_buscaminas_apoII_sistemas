904ec0fd50afdda58235354666681e391cb1775d
Felipe Garcia <felipegarcial64@gmail.com>
2019-08-11 23:50:20 -0500

    Se agregan los test

--- src/modelo/Buscaminas.java
+++ src/modelo/Buscaminas.java
@@ -158,7 +158,6 @@ public class Buscaminas {
 		generarMinas();
 		inicializarCasillasLibres();
 		inicializarValorCasillasLibres();
-
 	}
 
 	/**
---
+++ src/test/BuscaminasTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import junit.framework.TestCase;
+import modelo.Buscaminas;
+import modelo.Casilla;
+
+public class BuscaminasTest extends TestCase{
+	private Buscaminas buscaminas;
+	private Casilla casilla;
+	
+	
+}
